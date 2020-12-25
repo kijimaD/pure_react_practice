6773f3b720c783ab8382cf026146db631fc3c309
kijima <norimaking777@gmail.com>
2020-12-26 01:11:25 +0900

    Add cart pages

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,32 +1,63 @@
 import React, { useState } from 'react';
 import Nav from './Nav';
 import ItemPage from './ItemPage';
+import CartPage from './CartPage';
 import './App.css';
 import { items } from './static-data';
 
 const App = () => {
     const [activeTab, setActiveTab] = useState('items');
+    const [cart, setCart] = useState([]);
+
+    const addToCart = item => {
+        setCart(prevCart => [...prevCart, item]);
+    };
 
     return (
 	<div className="App">
 	  <Nav
             activeTab={activeTab}
-            onTabChange={setActiveTab}/>
+            onTabChange={setActiveTab}
+          />
 	  <main className="App-content">
-            <Content />
+            <Content
+              tab={activeTab}
+              onAddToCart={addToCart}
+              cart={summarizeCart(cart)}
+            />
           </main>
         </div>
     );
 };
 
-const Content = ({ tab }) => {
+const Content = ({ tab, onAddToCart, cart }) => {
     switch (tab) {
     default:
     case 'items':
-        return <ItemPage items={items} />;
+        return (
+            <ItemPage
+              items={items}
+              onAddToCart={onAddToCart}
+            />
+        );
     case 'cart':
-        return <span>the cart</span>;
+        return <CartPage items={cart} />;
     }
+};
+
+const summarizeCart = cart => {
+    const groupedItems = cart.reduce((summary, item) => {
+        summary[item.id] = summary[item.id] || {
+            ...item,
+            count: 0
+        };
+
+        summary[item.id].count++;
+
+        return summary;
+    }, {});
+
+    return Object.values(groupedItems);
 };
 
 export default App;
