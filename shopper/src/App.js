c572da9ef5992f71b79a04485bdd7d788b2eefd2
kijima <norimaking777@gmail.com>
2020-12-26 21:38:55 +0900

    Finish example

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -13,6 +13,53 @@ const App = () => {
         setCart(prevCart => [...prevCart, item]);
     };
 
+    const Content = ({ tab, onAddToCart, onRemoveItem, cart }) => {
+        switch (tab) {
+        default:
+        case 'items':
+            return (
+                <ItemPage
+                  items={items}
+                  onAddToCart={onAddToCart}
+                />
+            );
+        case 'cart':
+            return (
+                <CartPage
+                  items={cart}
+                  onAddOne={onAddToCart}
+                  onRemoveOne={onRemoveItem}
+                />
+            );
+        }
+    };
+
+    const summarizeCart = cart => {
+        const groupedItems = cart.reduce((summary, item) => {
+            summary[item.id] = summary[item.id] || {
+                ...item,
+                count: 0
+            };
+
+            summary[item.id].count++;
+
+            return summary;
+        }, {});
+
+        return Object.values(groupedItems);
+    };
+
+    const removeItem = item => {
+        let index = cart.findIndex(i => i.id === item.id);
+        if (index >= 0) {
+            setCart(cart => {
+                const copy = [...cart];
+                copy.splice(index, 1);
+                return copy;
+            });
+        }
+    };
+
     return (
 	<div className="App">
 	  <Nav
@@ -23,6 +70,7 @@ const App = () => {
             <Content
               tab={activeTab}
               onAddToCart={addToCart}
+              onRemoveItem={removeItem}
               cart={summarizeCart(cart)}
             />
           </main>
@@ -30,34 +78,6 @@ const App = () => {
     );
 };
 
-const Content = ({ tab, onAddToCart, cart }) => {
-    switch (tab) {
-    default:
-    case 'items':
-        return (
-            <ItemPage
-              items={items}
-              onAddToCart={onAddToCart}
-            />
-        );
-    case 'cart':
-        return <CartPage items={cart} />;
-    }
-};
 
-const summarizeCart = cart => {
-    const groupedItems = cart.reduce((summary, item) => {
-        summary[item.id] = summary[item.id] || {
-            ...item,
-            count: 0
-        };
-
-        summary[item.id].count++;
-
-        return summary;
-    }, {});
-
-    return Object.values(groupedItems);
-};
 
 export default App;
