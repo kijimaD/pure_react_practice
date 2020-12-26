98db669c87204fc33a5223c29ec6edab062bad34
kijima <norimaking777@gmail.com>
2020-12-26 21:38:55 +0900

    Finish example

--- shopper/src/ItemPage.js
+++ shopper/src/ItemPage.js
@@ -8,9 +8,13 @@ function ItemPage({ items, onAddToCart }) {
 	<ul className="ItemPage-items">
           {items.map(item =>
                      <li key={item.id} className="ItemPage-item">
-                       <Item
-                         item={item}
-                         onAddToCart={() => onAddToCart(item)} />
+                       <Item item={item}>
+                         <button
+                           className="item-addToCart"
+                     onClick={() => onAddToCart(item)}>
+                           Add to Cart
+                         </button>
+                       </Item>
                      </li>
                     )}
         </ul>
