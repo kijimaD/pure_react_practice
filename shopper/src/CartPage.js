972ebe02ce708206ef814799603ed76840df5736
kijima <norimaking777@gmail.com>
2020-12-27 20:16:18 +0900

    Add total price

--- shopper/src/CartPage.js
+++ shopper/src/CartPage.js
@@ -4,24 +4,32 @@ import Item from './Item';
 import './CartPage.css';
 
 function CartPage({ items, onAddOne, onRemoveOne }) {
+    let totalPrice = 0;
+
     return (
+        <div>
         <ul className="CartPage-items">
-          {items.map(item =>
-                     <li key={item.id} className="CartPage-item">
-                       <Item item={item}>
-                         <div className="CartItem-controls">
-                           <button
-                             className="CartItem-removeOne"
-                             onClick={() => onRemoveOne(item)}>&ndash;</button>
-                           <span className="CartItem-count">{item.count}</span>
-                           <button
-                             className="CartItem-addOne"
-                             onClick={() => onAddOne(item)}>+</button>
-                         </div>
-                       </Item>
-                     </li>
-                    )}
+        {items.map(item =>
+                   <li key={item.id} className="CartPage-item">
+                   <Item item={item}>
+                   <div className="CartItem-controls">
+                   <button
+                   className="CartItem-removeOne"
+                   onClick={() => onRemoveOne(item)}>&ndash;</button>
+                   <span className="CartItem-count">{item.count}</span>
+                   <button
+                   className="CartItem-addOne"
+                   onClick={() => onAddOne(item)}>+</button>
+                   </div>
+                   </Item>
+                   </li>
+                  )}
         </ul>
+	{items.map(item => {
+	    totalPrice += item.price * item.count
+	})}
+        Total: ${totalPrice}
+        </div>
     );
 }
 CartPage.propTypes = {
